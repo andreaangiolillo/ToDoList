c583cac911d959d10ed9b59d831757dceaa37aa2
andreaangiolillo <angiolillo.andrea@gmail.com>
2019-06-30 17:30:12 +0100

    Added App class

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,25 @@ import CheckBox from './CheckBox';
 import NavigationBar from './NavigationBar';
 import todosData from '../data/testData.js';
 
-const todoList = todosData.map(item => (<CheckBox key={item.id} item={item} />));
 
-function App() {
-  return (
-    <div className="MainContainer" className="todo-list">
-      <NavigationBar />
-      <div>
-        {todoList}
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      todoList: todosData.map(item => (<CheckBox key={item.id} item={item} />))
+    }
+  }
+
+  render(){
+    return(
+      <div className="MainContainer" className="todo-list">
+        <NavigationBar />
+        <div>
+          {this.state.todoList}
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
